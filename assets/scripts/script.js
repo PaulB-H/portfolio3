c88f49bbc3f1de0fa849569d17ab896e7028d311
Paul B-H <51964537+PaulB-H@users.noreply.github.com>
2019-10-22 02:51:04 -0400

    Uploading portfolio 3

---
+++ assets/scripts/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    $('.parallax').parallax();
+    console.log("This is working");
+});
+
+$(document).ready(function () {
+    $('.carousel').carousel();
+    console.log("This is working");
+});
+
+
+// $(document).ready(function () {
+//     $("ul").click(function () {
+//         console.log("Clicked a nav item");
+//         $(".navSpacer").toggleClass("hide", false);
+//     });
+// });
+
+// $(document).ready(function () {
+//     $(window).scroll(function () {
+//         $(".navSpacer").toggleClass("hide");
+//         console.log("Hidden", true);
+//     });
+// });
---
+++ readme.md
@@ -0,0 +1,5 @@
+This is my third portfolio assignment.
+
+I wanted to rebuild it using materialize since we used it for our first project.
+
+I had some more issues with how custom styling works with the prebuild materialize elements, and had to change the navbar style I was going to go for, overall I am happy with the minimal design and neat parallax effect.
